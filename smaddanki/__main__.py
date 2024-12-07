34c2887b8c8af4290b76379d6f037ae53eb8a24d
Sudhamshu Addanki <sudhamshuaddanki@gmail.com>
2024-12-07 01:47:46 +0000

    complete repo clean-up

--- smaddanki/__main__.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-from .create_username import generate_username
-
-
-username = generate_username(sys.argv[1], sys.argv[2])
-
-
-print(username)
--- smaddanki/create_username.py
+++
@@ -1,4 +0,0 @@
-def generate_username(first_name: str, last_name: str) -> str:
-    """This function generated the username based on first name and last name"""
-
-    return f'{first_name}.{last_name}@smaddanki.com'
